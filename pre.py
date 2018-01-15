584dbc47492c79d1859564e88552ac21cfb80f82
dan <dgk89@hotmail.dk>
2018-01-15 17:37:03 +0100

    pre.py

--- pre.py
+++ pre.py
@@ -1,10 +1,8 @@
-with open('titles.txt') as infile:
-	titles = infile.read().split('\n')
+with open('data/links-simple-sorted.txt') as infile:
+	pages = [[int(num) for num in e.replace(':','').split(' ')] for e in infile.read().split('\n')]
 
-with open('pages.txt') as infile:
-	pages = [e.replace(':','').split(' ') for e in infile.read().split('\n')[:-1]]
 
-print(pages)
+fromTo 			= [(e[0],out) for e in pages for out in e[1:]]
+frm, to, deg 	= [e[0] for e in fromTo],[e[1] for e in fromTo],[len(e)-1 for e in pages]
 
-a = [1,2,3]
-del a
+
